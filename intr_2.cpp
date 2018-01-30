90fc9c7dc250105b46a91689c0a0b2464ff131b6
Mitradip Das <mitradip.das@niser.ac.in>
2018-01-31 00:28:42 +0530

    Update intr_2.cpp

--- intr_2.cpp
+++ intr_2.cpp
@@ -7,10 +7,10 @@ For documentation, please look into doc.html
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  string pdb_name="/home/mitradip/Work/Deep Eutactic Mixtures/Urea Choline Chloride/Classical/lammps_stuff/simulation/413K/last_1000_nvt.pdb";
+  string pdb_name="Data/sample_1000_frames.pdb";
   long int st=line_skip(pdb_name,0,1);
   trajectory tt;
-  long int pp=tt.read_frames(pdb_name,st,1000,57.852905);
+  long int pp=tt.read_frames(pdb_name,st,1000,50.0);
   std::vector<array<long,2>> v;
   tt.intr_cnt_3("C2","H4","CL",0.9,1.5,2.0,2.7,90,180,v);
   for(int i=0;i<v.size();++i) {
