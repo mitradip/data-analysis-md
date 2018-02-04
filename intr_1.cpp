19d957c3f0b652860926e6f49ca511df084034ee
Mitradip Das <mitradip.das@niser.ac.in>
2018-02-04 19:59:11 +0530

    Update and rename intr_2.cpp to intr_1.cpp

--- intr_1.cpp
+++ intr_1.cpp
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[]) {
   trajectory tt;
   long int pp=tt.read_frames(pdb_name,st,1000,50.0);
   std::vector<array<long,2>> v;
-  tt.intr_cnt_3("C2","H4","CL",0.9,1.5,2.0,2.7,90,180,v);
+  tt.intr_cnt_3("C","H","O",0.9,1.5,1.5,1.8,0,180,v);
   for(int i=0;i<v.size();++i) {
     std::cout << (v[i][0]*5) << " " << v[i][1] << '\n';
   }
